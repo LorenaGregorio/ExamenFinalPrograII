f6efda5965e17e8832f58cad06dae221a28b110c
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-03-30 17:38:53 -0600

    Agregar archivos de proyecto.

---
+++ TIENDA_EXAMENFINALPROII/TIENDA/CLIENTE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_EXAMENFINALPROII.TIENDA
+{
+    class CLIENTE
+    {
+    }
+}
