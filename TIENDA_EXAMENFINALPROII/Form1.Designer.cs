293187687ca27bf9aca2a87516b32e89d04ec86a
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-03-30 18:41:31 -0600

    c

--- TIENDA_EXAMENFINALPROII/Form1.Designer.cs
+++ TIENDA_EXAMENFINALPROII/Form1.Designer.cs
@@ -29,12 +29,22 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtprom = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -49,11 +59,47 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cliente";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Carrera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Apellidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombres";
+            // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(465, 70);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtprom);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(377, 70);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(248, 300);
+            this.groupBox2.Size = new System.Drawing.Size(376, 300);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tiempo Promedio";
@@ -67,32 +113,91 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "TIENDA";
             // 
-            // label2
+            // button1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nombres";
+            this.button1.Location = new System.Drawing.Point(41, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BEBIDAS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
+            // button2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 119);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Apellidos";
+            this.button2.Location = new System.Drawing.Point(149, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "COMIDA CHATARRA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label4
+            // button3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 180);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Carrera";
+            this.button3.Location = new System.Drawing.Point(262, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GALLETAS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(91, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ALMUERZOS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(208, 111);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "OTROS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(65, 182);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "PROMEDIO";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // txtprom
+            // 
+            this.txtprom.Location = new System.Drawing.Point(176, 184);
+            this.txtprom.Name = "txtprom";
+            this.txtprom.Size = new System.Drawing.Size(133, 20);
+            this.txtprom.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "PRODUCTOS";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(138, 233);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(134, 31);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "OTRO PROMEDIO";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
@@ -106,6 +211,8 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,6 +226,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtprom;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- TIENDA_EXAMENFINALPROII/Form1.cs
+++ TIENDA_EXAMENFINALPROII/Form1.cs
@@ -12,9 +12,138 @@ namespace TIENDA_EXAMENFINALPROII
 {
     public partial class Form1 : Form
     {
+
+        public string valorInicial = "";
+        public string valorFinal = "";
+        public string signoActual = "";
+        public int contadorCalculos = 0;
+        public double promedio = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ReiniciarValores();
+            txtprom.Text = "";
+
+        }
+
+
+        public void AgregarValorInicial(string valor)
+        {
+            valorInicial += valor;
+            txtprom.Text = valorInicial.ToString();
+        }
+
+        
+        public void agregarValorFinal(string valor)
+        {
+            valorFinal += valor;
+            txtprom.Text = valorFinal.ToString();
+        }
+
+
+        private void ReiniciarValores()
+        {
+            signoActual = "";
+            valorInicial = "";
+            valorFinal = "";
+            contadorCalculos = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((signoActual != "") && (contadorCalculos == 0))
+            {
+                AgregarValorInicial("1");
+            }
+            else
+            {
+                agregarValorFinal("1");
+            }
+            //    btnigual.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((signoActual != "") && (contadorCalculos == 0))
+            {
+                AgregarValorInicial("5");
+            }
+            else
+            {
+                agregarValorFinal("5");
+            }
+            //btnigual.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ((signoActual != "") && (contadorCalculos == 0))
+            {
+                AgregarValorInicial("2");
+            }
+            else
+            {
+                agregarValorFinal("2");
+            }
+            //btnigual.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((signoActual != "") && (contadorCalculos == 0))
+            {
+                AgregarValorInicial("10");
+            }
+            else
+            {
+                agregarValorFinal("10");
+            }
+            //btnigual.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if ((signoActual != "") && (contadorCalculos == 0))
+            {
+                AgregarValorInicial("3");
+            }
+            else
+            {
+                agregarValorFinal("3");
+            }
+            //btnigual.Focus();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double promedio = 0;
+            switch (signoActual)
+            {
+                case "PROMEDIO":
+                    promedio = (Convert.ToDouble(valorInicial) + Convert.ToDouble(valorFinal));
+                    txtprom.Text = promedio.ToString();
+                    //    btnigual.Focus();
+
+                    break;
+                    //case "*":
+                    //    resultado = Convert.ToDouble(valorInicial) * Convert.ToDouble(valorFinal);
+                    //    PANTALLA.Text = resultado.ToString();
+                    //    break;
+                    //case "-":
+                    //    resultado = Convert.ToDouble(valorFinal) - Convert.ToDouble(valorInicial);
+                    //    PANTALLA.Text = resultado.ToString();
+                    //    break;
+                    //case "/":
+                    //    resultado = Convert.ToDouble(valorFinal) / Convert.ToDouble(valorInicial);
+                    //    PANTALLA.Text = resultado.ToString();
+                    //    break;
+            }
+
         }
     }
 }
